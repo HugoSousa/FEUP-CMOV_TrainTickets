6a1495dbb3cfaeedbed6f0c6eee72653b9298220
Francisco Maciel <francisco.ma.maciel@gmail.com>
2015-11-09 17:20:36 +0000

    Add NFC

--- InspectorTrainTicketingClient/app/src/main/java/feup/comv/inspectortrainticketingclient/RouteActivity.java
+++ InspectorTrainTicketingClient/app/src/main/java/feup/comv/inspectortrainticketingclient/RouteActivity.java
@@ -10,17 +10,29 @@ import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.X509EncodedKeySpec;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -32,6 +44,13 @@ public class RouteActivity extends AppCompatActivity implements OnApiRequestComp
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Toast.makeText(getApplicationContext(), "NFC attempted. Open the current route to validate tickets.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_route);
 
         ApiRequest request = new ApiRequest(ApiRequest.GET, this, ApiRequest.requestCode.STATIONS, null);
@@ -101,7 +120,240 @@ public class RouteActivity extends AppCompatActivity implements OnApiRequestComp
         };
 
         registerReceiver(br, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+
     }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        if (route != null) {
+            Log.d("cenas2",route.getKey());
+        }
+        Log.d("cenas2", new String(msg.getRecords()[0].getPayload()));
+    }
+
+    public void validateResult(String result) {
+        String routeString = route.getKey();
+
+        JSONObject ticketObj = null;
+        try {
+            ticketObj = new JSONObject(result);
+            JSONObject dataObj = ticketObj.getJSONObject("data");
+            String code = dataObj.getString("code");
+            String signature = ticketObj.getString("signature");
+
+            SharedPreferences sp2 = getSharedPreferences("login", 0);
+            SharedPreferences sp = getSharedPreferences("routes", 0);
+            String public_string = sp2.getString("pub", null);
+
+
+            HashSet<String> tickets = (HashSet<String>) sp.getStringSet(routeString, null);
+            boolean found = false;
+            boolean validated = false;
+            String ticketCode = null;
+            for (String ticket: tickets) {
+
+                JSONObject tObj = new JSONObject(ticket);
+                ticketCode =  tObj.getString("uuid");
+                if (ticketCode.equals(code)) {
+                    Log.d("cenas", "Local match:");
+                    Log.d("cenas", ticket);
+                    //String tSig = tObj.getString("signature");
+                    String tRouteId = tObj.getString("route_id");
+                    String userId = tObj.getString("user_id");
+                    String dateString = tObj.getString("route_date");
+
+                    SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                    Date date = dt.parse(dateString);
+
+                    SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String properDate = dt1.format(date);
+
+                    String mask = code + " " + tRouteId + " " + userId + " " + properDate;
+
+                    //remove comment lines from public_key
+                    String[] publicKeySplit = public_string.split("\n");
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < publicKeySplit.length - 1; i++) {
+                        if(! publicKeySplit[i].startsWith("-----"))
+                            sb.append(publicKeySplit[i]);
+                    }
+                    String publicKeyString = sb.toString();
+
+                    PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.decode(publicKeyString.getBytes(), Base64.DEFAULT)));
+                    //PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.decode(publicKeyString.getBytes("utf-8"), Base64.DEFAULT)));
+                    //http://stackoverflow.com/questions/11532989/android-decrypt-rsa-text-using-a-public-key-stored-in-a-file
+
+                    Signature sg = Signature.getInstance("SHA1WithRSA");
+
+
+                    sg.initVerify(publicKey);
+                    sg.update(mask.getBytes());
+                    validated = sg.verify(Base64.decode(signature.getBytes(), Base64.DEFAULT));
+                    Log.d("cenas", "Resultado "+ validated);
+                    found = true;
+                    break;
+                }
+            }
+
+
+            if(found){
+                if(validated) {
+                    if(! isValidated(routeString, ticketCode)) {
+                        String fromToMessage = setTicketValidated(routeString, ticketCode);
+                        setValidation(true, fromToMessage);
+                    }else{
+                        setValidation(false, "Ticket already validated.");
+                    }
+                }else{
+                    addFraudulentTicketStatistic();
+                    setValidation(false, "Signature verify failed.");
+                }
+            }else{
+                addFraudulentTicketStatistic();
+                setValidation(false, "Invalid ticket code.");
+            }
+
+        }catch(Exception e){
+            addFraudulentTicketStatistic();
+            setValidation(false, e.toString());
+        }
+
+    }
+
+    private void setValidation(boolean validated, String reason) {
+        if (validated) {
+            ImageView iv = (ImageView)findViewById(R.id.ticket_validation_view);
+            iv.setColorFilter(Color.parseColor("#4caf50"));
+            iv.setImageResource(R.drawable.ic_navigation_check);
+            int validatedTickets = Integer.parseInt(((TextView) findViewById(R.id.text_validated_tickets)).getText().toString());
+            validatedTickets++;
+            ((TextView)findViewById(R.id.text_validated_tickets)).setText(Integer.toString(validatedTickets));
+
+            //check from where to where it is valid
+
+            ((TextView)findViewById(R.id.validation_info_tv)).setText(reason);
+
+        }else {
+            ImageView iv = (ImageView)findViewById(R.id.ticket_validation_view);
+            iv.setColorFilter(Color.parseColor("#f32313"));
+            iv.setImageResource(R.drawable.ic_navigation_close);
+            ((TextView)findViewById(R.id.validation_info_tv)).setText(reason);
+        }
+    }
+
+    private void addFraudulentTicketStatistic(){
+        SharedPreferences spStatistics = getSharedPreferences("statistics", 0);
+        SharedPreferences.Editor statisticsEditor = spStatistics.edit();
+        int fraudulent = spStatistics.getInt("fraudulent_tickets", 0);
+        statisticsEditor.putInt("fraudulent_tickets", fraudulent + 1);
+        statisticsEditor.commit();
+    }
+
+    private boolean isValidated(String routeKey, String ticketCode){
+
+        HashSet<String> tickets = null;
+        SharedPreferences sp = getSharedPreferences("routes", 0);
+        Map<String,?> keys = sp.getAll();
+
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+
+            if(entry.getKey().equals(routeKey)) {
+                tickets = (HashSet<String>) entry.getValue();
+                break;
+            }
+        }
+
+        try {
+            for(String ticket: tickets){
+                JSONObject ticketObj = new JSONObject(ticket);
+
+                String code = ticketObj.getString("uuid");
+                if(ticketCode.equals(code)){
+                    JSONArray validatedArray = ticketObj.getJSONArray("is_validated");
+                    if(validatedArray.get(0).equals(1))
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private String setTicketValidated(String routeKey, String ticketCode){
+        String fromToString = null;
+        String routeString = route.getKey();
+
+        JSONArray validatedArray = new JSONArray();
+        validatedArray.put(1);
+        SharedPreferences sp = getSharedPreferences("routes", 0);
+        Map<String,?> keys = sp.getAll();
+        System.out.println(keys);
+        HashSet<String> tickets = null;
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+
+            if(entry.getKey().equals(routeKey)) {
+                tickets = (HashSet<String>) entry.getValue();
+                break;
+            }
+        }
+
+        boolean found = false;
+        try {
+            for(String ticket: tickets){
+                JSONObject ticketObj = new JSONObject(ticket);
+
+                String code = ticketObj.getString("uuid");
+                if(ticketCode.equals(code)){
+                    found = true;
+                    //intent.putExtra("from", ticketObj.getInt("start_station"));
+                    //intent.putExtra("to", ticketObj.getInt("end_station"));
+                    int from = ticketObj.getInt("start_station");
+                    int to = ticketObj.getInt("end_station");
+                    fromToString = "Valid from " + getStationName(Integer.toString(from)) + " to " + getStationName(Integer.toString(to));
+
+                    //replace this ticket in the hashset
+                    tickets.remove(ticket);
+                    ticketObj.put("is_validated", validatedArray);
+                    tickets.add(ticketObj.toString());
+
+                    System.out.println(ticket);
+                    break;
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(found){
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putStringSet(routeString, tickets);
+            editor.commit();
+
+            SharedPreferences spStatistics = getSharedPreferences("statistics", 0);
+            SharedPreferences.Editor statisticsEditor = spStatistics.edit();
+            int validated = spStatistics.getInt("validated_tickets", 0);
+            statisticsEditor.putInt("validated_tickets", validated + 1);
+            statisticsEditor.commit();
+        }
+        return fromToString;
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- UserTrainTicketingClient/app/src/main/java/feup/cmov/TicketActivity.java
+++ UserTrainTicketingClient/app/src/main/java/feup/cmov/TicketActivity.java
@@ -2,10 +2,16 @@ package feup.cmov;
 
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
@@ -13,7 +19,10 @@ import com.google.zxing.WriterException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class TicketActivity extends AppCompatActivity {
+import java.nio.charset.Charset;
+
+public class TicketActivity extends AppCompatActivity implements NfcAdapter.OnNdefPushCompleteCallback  {
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +58,7 @@ public class TicketActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.ticket_qr_view);
 
-        String data = obj.toString();
+        data = obj.toString();
         QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(data, null,
                 "TEXT_TYPE", BarcodeFormat.QR_CODE.toString(), data.length());
 
@@ -76,5 +85,40 @@ public class TicketActivity extends AppCompatActivity {
         SharedPreferences sp = this.getSharedPreferences("stations", 0);
 
         return sp.getString(id, null);
+    }
+
+    public void sendNfc(View v) {
+        NfcAdapter mNfcAdapter;
+        String tag = "application/nfc.feup.cmov.tickettype";
+        byte[] message;
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(getApplicationContext(), "NFC is not available on this device.", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            message = data.getBytes();
+            NdefMessage msg = new NdefMessage(new NdefRecord[] { createMimeRecord(tag, message) });
+            mNfcAdapter.setNdefPushMessage(msg, this);
+            mNfcAdapter.setOnNdefPushCompleteCallback(this, this);
+            Toast.makeText(getApplicationContext(), "Ticket sending attempted...", Toast.LENGTH_LONG).show();
+
+
+        }
+    }
+
+
+    public NdefRecord createMimeRecord(String mimeType, byte[] payload) {
+        byte[] mimeBytes = mimeType.getBytes(Charset.forName("ISO-8859-1"));
+        return new NdefRecord(
+                NdefRecord.TNF_MIME_MEDIA, mimeBytes, new byte[0], payload);
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Ticket sent.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
